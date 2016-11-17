8a7476e4bcec1e2cf7f80cd886073073196a7aad
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2016-11-16 22:42:57 -0500

    Initial Commit

---
+++ nomad-wpilibj-lite/src/edu/wpi/first/wpilibj/RobotBase.java
@@ -0,0 +1,9 @@
+package edu.wpi.first.wpilibj;
+
+public abstract class RobotBase {
+	
+	
+	public static void main(String[] args) {
+		// Format of arguments: <Robot Class Name> <Endpoint URL> <Client ID>
+	}
+}
